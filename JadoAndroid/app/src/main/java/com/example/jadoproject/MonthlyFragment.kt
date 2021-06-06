09a608bf8ce41bb9040f7183c9179560fa4b07cf
ddyeon <cab05102@naver.com>
2021-06-07 02:05:08 +0900

    calendarview custom

--- JadoAndroid/app/src/main/java/com/example/jadoproject/MonthlyFragment.kt
+++ JadoAndroid/app/src/main/java/com/example/jadoproject/MonthlyFragment.kt
@@ -8,12 +8,21 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.jadoproject.databinding.FragmentMonthlyBinding
+import com.github.sundeepk.compactcalendarview.domain.Event
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 import java.util.*
 
 
 class MonthlyFragment : Fragment() {
     private lateinit var binding : FragmentMonthlyBinding
+
+    var simpleDateFormat : SimpleDateFormat = SimpleDateFormat("mmmm-yyyy", Locale.KOREA)
+    var DateFormat : SimpleDateFormat = SimpleDateFormat("yyyy-mm-dd", Locale.KOREA)
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,10 +30,42 @@ class MonthlyFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_monthly, container, false)
 
-        
 
+        setDate()
 
         return binding.root
+    }
+
+    //캘린더 버튼
+    fun calendarButton()
+    {
+
+    }
+
+    fun setDate()
+    {
+        val c = Calendar.getInstance().time
+        val df = SimpleDateFormat("yyyy-mm-dd")
+        val formattedDate = df.format(c)
+
+        binding.calanderView.setUseThreeLetterAbbreviation(true)
+
+        val sdf = SimpleDateFormat("MMMM yyyy")
+
+        val myCalendar = Calendar.getInstance()
+
+        myCalendar.set(Calendar.YEAR, 2021)
+        myCalendar.set(Calendar.MONTH, 6)
+        myCalendar.set(Calendar.DAY_OF_MONTH, 17 )
+
+
+
+
+        val event = Event(Color.RED, System.currentTimeMillis()+ 3600 * 1000, "Test")
+        binding.calanderView.addEvent(event)
+
+
+
     }
 
 
--- JadoAndroid/app/src/main/java/com/example/jadoproject/MypageFragment.kt
+++ JadoAndroid/app/src/main/java/com/example/jadoproject/MypageFragment.kt
@@ -7,6 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.jadoproject.data.DateInfo
+import com.example.jadoproject.data.ID
+import com.example.jadoproject.data.Info
+import com.example.jadoproject.data.Study
 import com.example.jadoproject.databinding.FragmentMypageBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.DataSnapshot
@@ -17,7 +21,8 @@ import com.google.firebase.database.ValueEventListener
 class MypageFragment : Fragment() {
 
     private lateinit var binding : FragmentMypageBinding
-
+    val FIREBASE_URL = "https://jadoproject-530a4-default-rtdb.asia-southeast1.firebasedatabase.app"
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance(FIREBASE_URL)
 
 
     override fun onCreateView(
@@ -27,12 +32,16 @@ class MypageFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_mypage, container,false)
 
+      /*  val info : ID = ID(Info("dayeon", "123", "01083726110", "ekdus2574@gmail.com"),
+            Study(DateInfo(3,100,))
+        )
+*/
 
 
         binding.cameraBtn.setOnClickListener {
             firebaseConnet()
             Log.d("click", "button")
-            //FirebaseDatabase.getInstance().reference.push().setValue("A" + 1);
+            //database.reference.child("ID").setValue(info)
 
         }
 
@@ -44,9 +53,8 @@ class MypageFragment : Fragment() {
 
     fun firebaseConnet()
     {
-         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("Camera")
 
+        val myRef = database.getReference("Camera")
 
         myRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
