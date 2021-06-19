ea2ef6568c76e03a808f132fd86ec6d210f90e17
ddyeon <cab05102@naver.com>
2021-06-19 23:14:11 +0900

    weekfragment
    - month에 view추가

--- JadoAndroid/app/src/main/java/com/example/jadoproject/WeekFragment.kt
+++ JadoAndroid/app/src/main/java/com/example/jadoproject/WeekFragment.kt
@@ -6,13 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.jadoproject.databinding.ActivityMainBinding
 import com.example.jadoproject.databinding.FragmentWeekBinding
+import com.example.jadoproject.databinding.ItemWeekBinding
 
 
 class WeekFragment : Fragment() {
     private lateinit var binding: FragmentWeekBinding
+
+    private var weekList  : ArrayList<ItemWeekBinding> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +27,26 @@ class WeekFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_week, container, false)
 
+
+        weekList = arrayListOf(binding.include2, binding.include3,binding.include4, binding.include5, binding.include6,
+        binding.include7, binding.include8)
+
+
+        setOnClick()
+
         return binding.root
+    }
+
+    fun setOnClick()
+    {
+        for( i in 0 until weekList.size)
+        {
+            weekList[i].weekconst.setOnClickListener {
+                parentFragmentManager.beginTransaction().replace(R.id.weekContainer,DailyFragment()).commit()
+          /*      val action = WeekFragmentDirections.actionWeekFragmentToDailyFragment()
+                findNavController().navigate(action)*/
+            }
+        }
     }
 
 
