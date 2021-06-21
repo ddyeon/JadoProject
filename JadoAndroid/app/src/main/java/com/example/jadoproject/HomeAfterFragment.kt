9b5e274858848dac2cc642451790ff20f5039493
yuuuuh <youh0909@gmail.com>
2021-06-21 13:39:57 +0900

    home&home-after
    home&home-after

--- JadoAndroid/app/src/main/java/com/example/jadoproject/HomeAfterFragment.kt
+++ JadoAndroid/app/src/main/java/com/example/jadoproject/HomeAfterFragment.kt
@@ -1,7 +1,40 @@
 package com.example.jadoproject
 
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.jadoproject.databinding.FragmentHomeAfterBinding
 
-class HomeAfterFragment {
 
-}
+class HomeAfterFragment : Fragment(){
+
+    private var time = 0
+    //private var timerTask: Timer ?= null
+
+    var flag = true
+
+    private lateinit var binding : FragmentHomeAfterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home_after, container, false)
+        return binding.root
+    }
+
+//
+//        binding.Stop_Btn.setOnClicklistener(View.OnClickListener {
+//            endRecord()
+//        })endRecord
+
+
+    }
+
 
